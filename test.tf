f6cdc51e08d79aed1f217c8d486070aa351e2185
srini <mareedu868@gmail.com>
2023-02-24 00:58:57 +0530

    terraform script

--- test.tf
+++ test.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami =  "ami-0a017d8ceb274537d"
   instance_type = "t2.micro"
-  vpc_security_groups = ["sg-051b2c7d5d6eb3df6"]
+  vpc_security_group_ids = ["sg-051b2c7d5d6eb3df6"]
   tags = {
     Name = "test"
   }
