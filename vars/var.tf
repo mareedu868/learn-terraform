ca68c3c1c1d41e2875c1349846dd71b8ae7e8bda
srini <mareedu868@gmail.com>
2023-02-25 11:18:40 +0530

    variable script

--- vars/var.tf
+++ vars/var.tf
@@ -1,7 +1,14 @@
-variable "sample1" {
+variable "sample" {
   default = "DevOps"
 }
 
+output "sample" {
+  value=var.sample
+  value=var.sample1
+}
+
+variable "sample1" {}
 output "sample1" {
   value=var.sample1
 }
+
